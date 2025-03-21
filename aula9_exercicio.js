8892212b3445fcce4f81e22d186f0574363dce6f
alyfermts <alyferm24@gmail.com>
2025-03-21 18:53:33 -0300

    Add files via upload

--- aula9_exercicio.js
+++ aula9_exercicio.js
@@ -5,3 +5,17 @@ for (let i = 0; i < numeros.length; i++) {
     soma += Number(numeros[i]);
 }
 console.log(soma)
+
+function ordenarString(arr) {
+    return arr.sort();
+}
+
+let frutas2 = ["banana","maçã","laranja","abacte"];
+console.log("frutas ordenadas:", ordenarString(frutas2));
+
+function removerduplicados(arr) {
+    return [...new Set(arr)];
+}
+
+let numeros3 = [1,2,2,3,4,4,];
+console.log("Sem duplicados:", removerduplicados(numeros3));
