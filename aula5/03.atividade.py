7842de84c6f98fb67a3248bd1a6a4d871997c2c6
alyfermts <alyferm24@gmail.com>
2025-05-09 21:22:24 -0300

    Add files via upload

---
+++ aula5/03.atividade.py
@@ -0,0 +1,32 @@
+# Atividade Prática: Sistema de uma Biblioteca
+'''
+Contexto:
+Você foi contratado para desenvolver um sistema de gerenciamento de biblioteca usando POO em Python.
+O sistema deve modelar livros, usuários e empréstimos, com funcionalidades básicas de cadastro, consulta e operações.
+
+Requisitos:
+- O sistema deve permitir o cadastro de livros, usuários e empréstimos.
+- O sistema deve permitir a consulta de livros cadastrados.
+- O sistema deve permitir a consulta de usuários cadastrados.
+'''
+
+# DESAFIO (opcional) Classe LivroDigital:
+# Herde de Livro e adicione o formato do e-book e formas de download.
+
+
+
+class cadastro:
+     def __init__(self, nome,id):
+          self.nome = nome
+          self.id = id 
+
+
+class biblioteca:
+
+     def __init__ (self, livros,  modelar_livros):
+          self.livros = livros
+          self.modelar_livros = modelar_livros
+          
+
+
+
