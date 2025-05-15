4e1f562d512bdd878fa96dfdb0db43421fdc4384
alyfermts <alyferm24@gmail.com>
2025-05-14 21:19:54 -0300

    Delete app02 directory

--- app02/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def home(request):
-    return render(request, 'home.html')
-def contatos(request):
-    return render(request, 'contatos.html')
-def produtos(request):
-    return render(request, 'produtos.html')
