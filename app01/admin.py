b53862cfd3a7dcd517deef7e08c3e1930e9987cc
alyfermts <alyferm24@gmail.com>
2025-05-28 21:27:10 -0300

    Add files via upload

--- app01/admin.py
+++ app01/admin.py
@@ -2,5 +2,3 @@ from django.contrib import admin
 from .models import Produto
 
 admin.site.register(Produto)
-
-
--- senacrj/settings.py
+++ senacrj/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app01',
-    'app02',
+    
 ]
 
 MIDDLEWARE = [
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+  
 
 
 STATICFILES_DIRS = [
@@ -129,6 +130,9 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
